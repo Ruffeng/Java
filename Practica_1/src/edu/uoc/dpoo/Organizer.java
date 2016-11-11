e73ea692bd9444f114c106b078ff0ddf50a498e2
Ruffeng <ruben.suet@gmail.com>
2016-11-11 09:59:46 +0100

    Doing a huge excercise

---
+++ Practica_1/src/edu/uoc/dpoo/Organizer.java
@@ -0,0 +1,23 @@
+package edu.uoc.dpoo;
+public class Organizer extends User {
+
+	public Organizer(String user, String pass, String fullName) {
+		super(user, pass, fullName);
+	}
+
+	public void removeSubmission(Submission submission) {
+	}
+
+	public boolean sendMessage(Competition competition, String subject,
+			String message) {
+		return false;
+	}
+
+	public Competition newCompetition(String title, Float target) {
+		return null;
+	}
+
+	public void closeCompetition(Competition competition) {
+	}
+
+}
